d1da85beacc1835e44ce6b041f8bca71f2ad07af
Ire Aderinokun <ire@ireaderinokun.com>
2015-07-27 12:02:23 +0100

    Made watchface fullscreen

--- src/app.js
+++ src/app.js
@@ -1,17 +1,14 @@
 /**
- * Welcome to Pebble.js!
- *
- * This is where you write your app.
+ * HTML5 Time - Pebble.js Watchface
  */
 
 var UI = require('ui');
 var Vector2 = require('vector2');
 
-var window = new UI.Window();
-
+var window = new UI.Window({ fullscreen: true });
 
 var openTag = new UI.Text({
-  position: new Vector2(0, 0),
+  position: new Vector2(0, 10),
   size: new Vector2(144, 168),
   text:'<time>',
   font:'GOTHIC_14_BOLD',
@@ -22,7 +19,7 @@ var openTag = new UI.Text({
 });
 
 var closeTag = new UI.Text({
-  position: new Vector2(0, 120),
+  position: new Vector2(0, 140),
   size: new Vector2(144, 168),
   text:'</time>',
   font:'GOTHIC_14_BOLD',
@@ -33,7 +30,7 @@ var closeTag = new UI.Text({
 });
 
 var time = new UI.TimeText({
-  position: new Vector2(0, 25),
+  position: new Vector2(0, 40),
   size: new Vector2(144, 168),
   text:'%I:%M',
   font:'BITHAM_42_LIGHT',
@@ -45,7 +42,7 @@ var time = new UI.TimeText({
 });
 
 var date = new UI.TimeText({
-  position: new Vector2(0, 75),
+  position: new Vector2(0, 90),
   size: new Vector2(144, 168),
   text:'%a %d %b',
   font:'GOTHIC_24',
@@ -56,7 +53,7 @@ var date = new UI.TimeText({
   
 });
 
-// Add elements to screen
+
 window.add(openTag);
 window.add(time);
 window.add(date);
